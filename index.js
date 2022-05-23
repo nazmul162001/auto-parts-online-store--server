f35811e16d804d19031bd7a4641bce40ecdbf383
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-23 14:09:21 +0600

    connect server to database & create user data

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
-require('dotenv').config()
-const app = express()
+require('dotenv').config();
+const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+// URI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nzql2.mongodb.net/?retryWrites=true&w=majority`;
+
+// Client
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+
+async function run(){
+
+  try{
+    await client.connect();
+    console.log('database connected');
+  }
+  finally{
+
+  }
+  
+}
+
+run().catch(console.dir)
+
+
 
 app.get('/', (req, res) => {
-  res.send('Hello from Online Parts store!')
-})
+  res.send('Hello from Online Parts store!');
+});
 
 app.listen(port, () => {
-  console.log(`Parts store app listening on port ${port}`)
-})
+  console.log(`Parts store app listening on port ${port}`);
+});
