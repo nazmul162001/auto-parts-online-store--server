e87c4b3d218a52da88728874c94df3203861f3b7
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-24 14:35:52 +0600

    create api for give access user to admin role

--- index.js
+++ index.js
@@ -56,6 +56,18 @@ async function run() {
       res.send(users);
     });
 
+    // api for user admin role
+    app.put('/user/admin/:email', async (req, res) => {
+      const email = req.params.email;
+
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: 'admin' },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     // api for insert user login information to database
     app.put('/user/:email', async (req, res) => {
       const email = req.params.email;
