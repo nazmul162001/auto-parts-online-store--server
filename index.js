871778e75f4512a31df4be3c0bd2501b7494e4fb
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-24 11:40:49 +0600

    create jwt for user login

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -44,7 +45,12 @@ async function run() {
         $set: user,
       };
       const result = await userCollection.updateOne(filter, updateDoc, options);
-      res.send(result);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: '1d' }
+      );
+      res.send({ result, token });
     });
 
     // api for see single item info
