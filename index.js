1224ffc3b552ad717b9f2b510ecadcb4cdcf653a
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-24 16:40:01 +0600

    create api for admin route

--- index.js
+++ index.js
@@ -56,16 +56,32 @@ async function run() {
       res.send(users);
     });
 
+    // api for admin route
+    app.get('/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === 'admin';
+      res.send({ admin: isAdmin });
+    });
+
     // api for user admin role
     app.put('/user/admin/:email', async (req, res) => {
       const email = req.params.email;
-
-      const filter = { email: email };
-      const updateDoc = {
-        $set: { role: 'admin' },
-      };
-      const result = await userCollection.updateOne(filter, updateDoc);
-      res.send(result);
+      // verify admin
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === 'admin') {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: 'admin' },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      } else {
+        res.status(403).send({ message: 'Forbidden' });
+      }
     });
 
     // api for insert user login information to database
